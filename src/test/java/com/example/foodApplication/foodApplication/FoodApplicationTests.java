d13fc030ac749d376e8a7a27879a5fae3f9db4e0
aliayman-225 <85026929+aliayman-225@users.noreply.github.com>
2022-08-15 11:29:36 +0200

    Initial commit

---
+++ src/test/java/com/example/foodApplication/foodApplication/FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodApplication.foodApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
