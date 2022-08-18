eedb821b897c0af38b4be9e31402ae83ef87791c
aliayman-225 <85026929+aliayman-225@users.noreply.github.com>
2022-08-18 04:29:06 +0200

    Update ProductController.java

--- src/main/java/com/example/foodApplication/controller/ProductController.java
+++ src/main/java/com/example/foodApplication/controller/ProductController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/Home")
 public class ProductController {
